4e79049d0be7ccb577ea74861f5fcd3f59914bbd
funk0r <th.seebacher@gmail.com>
2025-02-23 13:00:18 +0000

    Created User Tables and Login Mask

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,6 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -15,24 +14,67 @@ SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostnam
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = 'dein_geheimes_schluessel'
 
-from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
 
 class Comment(db.Model):
-
     __tablename__ = "comments"
-
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('comments', lazy=True))
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "GET":
-         return render_template("main_page.html", comments=Comment.query.all())
+    if "user" not in session:
+        return redirect(url_for("login"))
+    user = User.query.filter_by(username=session["user"]).first()
+    if request.method == "POST":
+        comment = Comment(content=request.form["contents"], user_id=user.id)
+        db.session.add(comment)
+        db.session.commit()
+        return redirect(url_for('index'))
+    comments = Comment.query.order_by(Comment.id.desc()).all()
+    return render_template("main_page.html", comments=comments, username=user.username)
 
-    comment = Comment(content=request.form["contents"])
-    db.session.add(comment)
-    db.session.commit()
-    return redirect(url_for('index'))
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return "Fehler: Benutzername existiert bereits!"
+        hashed_password = generate_password_hash(password)
+        user = User(username=username, password_hash=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("login"))
+    return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password_hash, password):
+            session["user"] = username
+            return redirect(url_for("index"))
+        return "Falscher Benutzername oder Passwort!"
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
+if __name__ == '__main__':
+    app.run(debug=True)
