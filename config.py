21b9b3f566ad30a8df1fe4e28a4d30052006f533
funk0r <th.seebacher@gmail.com>
2025-03-20 08:54:06 +0000

    Passwörter aus config.py entfernt und auf Umgebungsvariablen umgestellt

--- config.py
+++ config.py
@@ -1,15 +1,17 @@
 # config.py
+from decouple import config
+
 class Config:
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-        username="funk0r",
-        password="Timbersaw2025!",
-        hostname="funk0r.mysql.pythonanywhere-services.com",
-        databasename="funk0r$kochrezepte",
+        username=config("DB_USERNAME"),
+        password=config("DB_PASSWORD"),
+        hostname=config("DB_HOSTNAME"),
+        databasename=config("DB_NAME"),
     )
     SQLALCHEMY_POOL_RECYCLE = 299
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'dein_geheimes_schluessel'
+    SECRET_KEY = config("SECRET_KEY")
     SQLALCHEMY_ENGINE_OPTIONS = {
         "pool_recycle": 280,
         "pool_pre_ping": True,
